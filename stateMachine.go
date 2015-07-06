9a51a49662f19347a5a1bf9fc6f99895d88b1d91
pojoin <hechuangqiang@gmail.com>
2015-07-06 11:34:47 +0800

    状态机添加transiton的方法

--- stateMachine.go
+++ stateMachine.go
@@ -1,14 +1,47 @@
 package gostateMachine
 
+import (
+	"errors"
+	"strings"
+)
+
 //状态机定义
-type StateMachine struct{
-	Name string
-	transitionMap	map[string]*Transition
+type StateMachine struct {
+	Name          string
+	transitionMap map[string]*Transition
 }
 
-
-func NewStateMachine(name string) StateMachine{
-	return StateMachine{Name : name}
+func NewStateMachine(name string) StateMachine {
+	return StateMachine{Name: name}
 }
 
+func (m *StateMachine) AddTransition(tName string, startState, nextState State) {
+	tName = strings.ToUpper(tName)
+	t := &Transition{
+		startState: startState,
+		nextState:  nextState,
+		Name:       tName,
+	}
+	m.transitionMap[tName] = t
+}
 
+//获取transition
+func (m *StateMachine) GetTransitionByName(tName string) (*Transition, error) {
+	tName = strings.ToUpper(tName)
+	if t, ok := m.transitionMap[tName]; ok {
+		return t, nil
+	} else {
+		return nil, errors.New("can not fond transition by name" + tName)
+	}
+}
+
+//获取trasintions
+func (m *StateMachine) GetTransitionByState(state State) []*Transition {
+	ts := make([]*Transition, 0)
+	for _, v := range m.transitionMap {
+		if v.startState.IsEquels(state) {
+			ts = append(ts, v)
+		}
+	}
+	return ts
+}
